b670b064be8ca341caaf2403fce0e51b4f242b39
VetUps <sercherufa@gmail.com>
2025-10-07 10:01:03 +0500

    Создание WPF проекта

---
+++ WPF/ImperialSanWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ImperialSanWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
