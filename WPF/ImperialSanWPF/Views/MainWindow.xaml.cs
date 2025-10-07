5c1f8ade50987023baa2e1a5d3ed1def8837fec6
VetUps <sercherufa@gmail.com>
2025-10-07 14:02:47 +0500

    Визуал окна авторизации

--- WPF/ImperialSanWPF/Views/MainWindow.xaml.cs
+++ WPF/ImperialSanWPF/Views/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ImperialSanWPF.Views.Pages;
 
 namespace ImperialSanWPF
 {
@@ -19,6 +20,11 @@ namespace ImperialSanWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new LoginPage());
         }
     }
 }
