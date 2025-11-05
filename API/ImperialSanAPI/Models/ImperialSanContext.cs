ddedd1580b5f4bc9b4422b328e1f4a9d21d720fe
VetUps <sercherufa@gmail.com>
2025-11-05 10:19:09 +0500

    Поптыка в докер

--- API/ImperialSanAPI/Models/ImperialSanContext.cs
+++ API/ImperialSanAPI/Models/ImperialSanContext.cs
@@ -32,7 +32,7 @@ public partial class ImperialSanContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=root;password=1234;database=imperial_san", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.42-mysql"));
+        => optionsBuilder.UseMySql("server=db;user=root;password=1234;database=imperial_san", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.42-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
